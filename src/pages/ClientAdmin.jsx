941a51f78f905eb337cd707fa33d1f790ca9d59c
Ke1Tran666 <ke1tran666@gmail.com>
2024-12-17 18:35:00 +0700

    page permissions

--- src/pages/ClientAdmin.jsx
+++ src/pages/ClientAdmin.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
 import ClientHeader from '../components/Clients/ClientHeader/ClientHeader';
 import { Outlet } from 'react-router-dom';
@@ -6,6 +7,23 @@ import { Outlet } from 'react-router-dom';
 function ClientAdmin() {
     const [activeMenu, setActiveMenu] = useState('Overview');
     const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const user = localStorage.getItem('user');
+        if (user) {
+            const parsedUser = JSON.parse(user);
+            const userRoles = parsedUser.roles || [];
+
+            // Kiểm tra xem người dùng có chỉ 1 vai trò và vai trò đó là student không
+            if (userRoles.length === 1 && userRoles[0].roleName === 'Student') {
+                navigate('/'); // Điều hướng ra trang khác nếu chỉ có 1 vai trò là student
+            }
+        } else {
+            // Nếu không có người dùng, điều hướng về trang đăng nhập
+            navigate('/signin');
+        }
+    }, [navigate]);
 
     return (
         <div className="flex h-screen bg-gray-50 w-full">
@@ -29,4 +47,3 @@ function ClientAdmin() {
 }
 
 export default ClientAdmin;
-
